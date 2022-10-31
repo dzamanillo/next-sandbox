e8c13a91ada831248fe4179717afddd1c3f0b1bf
Daniel Zamanillo <dzamanillok@gmail.com>
2022-10-31 18:12:54 -0400

    pages/index: mark stage

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 			</Head>
 
 			<main className={styles.main}>
-				<h1 className={styles.title}>Welcome to the Main Branch</h1>
+				<h1 className={styles.title}>Welcome to the Stage Branch</h1>
 
 				<p className={styles.description}>
 					Get started by editing{" "}
