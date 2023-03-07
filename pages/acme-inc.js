d126bba80587ba5cbcb34504e51431d8ac0d1b75
Daniel Zamanillo <dzamanillok@gmail.com>
2023-03-07 17:52:19 -0500

    update to fix build

--- pages/acme-inc.js
+++ pages/acme-inc.js
@@ -3,9 +3,9 @@ export default function Acme() {
 		<>
 			<div id="form">
 				<iframe
-					class="clickup-embed clickup-dynamic-height"
+					className="clickup-embed clickup-dynamic-height"
 					src="https://forms.clickup.com/9010044376/f/8cgmqer-1500/X74O07PG4Z6H6BVK66"
-					onwheel=""
+					onWheel=""
 					width="100%"
 					height="100%"
 					style={{ background: "transparent", border: "1px solid #ccc" }}
@@ -17,9 +17,9 @@ export default function Acme() {
 			</div>
 			<div id="board">
 				<iframe
-					class="clickup-embed"
+					className="clickup-embed"
 					src="https://sharing.clickup.com/9010044376/b/h/8cgmqer-1480/0ddc0aca67f10f5"
-					onwheel=""
+					onWheel=""
 					width="100%"
 					height="700px"
 					style={{ background: "transparent", border: "1px solid #ccc" }}
