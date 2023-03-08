9020372f0eef4af7d30398a02a4f3553b1986031
Daniel Zamanillo <dzamanillok@gmail.com>
2023-03-08 11:22:56 -0500

    update order and add example of form documentation

--- pages/acme-inc.js
+++ pages/acme-inc.js
@@ -1,20 +1,6 @@
 export default function Acme() {
 	return (
-		<>
-			<div id="form">
-				<iframe
-					className="clickup-embed clickup-dynamic-height"
-					src="https://forms.clickup.com/9010044376/f/8cgmqer-1500/X74O07PG4Z6H6BVK66"
-					onWheel=""
-					width="100%"
-					height="100%"
-					style={{ background: "transparent", border: "1px solid #ccc" }}
-				></iframe>
-				<script
-					async
-					src="https://app-cdn.clickup.com/assets/js/forms-embed/v1.js"
-				></script>
-			</div>
+		<div>
 			<div id="board">
 				<iframe
 					className="clickup-embed"
@@ -22,9 +8,55 @@ export default function Acme() {
 					onWheel=""
 					width="100%"
 					height="700px"
-					style={{ background: "transparent", border: "1px solid #ccc" }}
+					style={{
+						background: "transparent",
+						border: "1px solid #ccc",
+					}}
 				></iframe>
 			</div>
-		</>
+			<div>
+				<h2 style={{ padding: "1rem", textAlign: "center" }}>
+					Ticket Documentation
+				</h2>
+				<div
+					style={{ display: "flex", justifyContent: "center", padding: "1rem" }}
+				>
+					<div>
+						<h3>Ticket Type</h3>
+						<ul>
+							<li>Bug: Something isn't working properly</li>
+							<li>Design: Make update to current design</li>
+							<li>New Feature: Request feature which will lead to discovery</li>
+						</ul>
+					</div>
+					<div>
+						<h3>Priority</h3>
+						<ul>
+							<li>Urgent: Blah Blah</li>
+							<li>High: Blah Blah</li>
+							<li>Normal: Blah Blah</li>
+							<li>Low: Blah Blah</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div id="form">
+				<iframe
+					className="clickup-embed clickup-dynamic-height"
+					src="https://forms.clickup.com/9010044376/f/8cgmqer-1500/X74O07PG4Z6H6BVK66"
+					onWheel=""
+					width="100%"
+					height="100%"
+					style={{
+						background: "transparent",
+						border: "none",
+					}}
+				></iframe>
+				<script
+					async
+					src="https://app-cdn.clickup.com/assets/js/forms-embed/v1.js"
+				></script>
+			</div>
+		</div>
 	);
 }
