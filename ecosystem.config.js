88af211a719d69bcd482ec5a45cdbe37eaf5c229
Daniel Zamanillo <dzamanillok@gmail.com>
2023-05-26 12:42:21 -0400

    node19

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,29 @@
+// module.exports = {
+// 	apps: [
+// 		{
+// 			name: "aws-codedeploy",
+// 			script: "npx",
+// 			args: "serve -s build -l 3000 -n",
+// 			interpreter: "none",
+// 			env: {
+// 				NODE_ENV: "development",
+// 			},
+// 		},
+// 	],
+// };
 module.exports = {
 	apps: [
 		{
-			name: "aws-codedeploy",
-			script: "npx",
-			args: "serve -s build -l 3000 -n",
-			interpreter: "none",
+			name: "NextjsApp",
+			script: "npm",
+			args: "start",
 			env: {
 				NODE_ENV: "development",
 			},
+			env_production: {
+				NODE_ENV: "production",
+			},
+			time: true,
 		},
 	],
 };
